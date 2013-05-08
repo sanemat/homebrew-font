1679de9ef74509a0b5efbd95b43fa0a1ea0ab633
sanemat <o.gata.ken@gmail.com>
2013-05-09 08:12:59 +0900

    Generate ricty_generator formula
    brew create https://github.com/yascentur/Ricty/archive/3.2.1.tar.gz

---
+++ ricty_generator.rb
@@ -0,0 +1,31 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class RictyGenerator < Formula
+  homepage ''
+  url 'https://github.com/yascentur/Ricty/archive/3.2.1.tar.gz'
+  sha1 '5a5aaa69949544eb8522ee1fa3d2862ce6a4a5e4'
+
+  # depends_on 'cmake' => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.j1  # if your formula's build system can't parallelize
+
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test ricty_generator`.
+    system "false"
+  end
+end
