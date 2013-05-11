cd3f52091da997da1011ac1cca163d84dc974589
sanemat <o.gata.ken@gmail.com>
2013-05-11 09:51:22 +0900

    Add migu1m

--- ricty.rb
+++ ricty.rb
@@ -5,6 +5,11 @@ class InconsolataFonts < Formula
   sha1 '7f0a4919d91edcef0af9dc153054ec49d1ab3072'
 end
 
+class Migu1MFonts < Formula
+  url 'http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Fmix-mplus-ipa%2F58720%2Fmigu-1m-20130430.zip'
+  sha1 '5fb8634d4f67df3889e98c1491e9d6b335bc95c7'
+end
+
 class Ricty < Formula
   homepage ''
   url 'https://github.com/yascentur/Ricty/archive/3.2.1.tar.gz'
