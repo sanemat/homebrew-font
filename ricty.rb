60cce6c510bae22d1744c25eb041d084fd10e1c8
sanemat <o.gata.ken@gmail.com>
2013-05-11 12:31:14 +0900

    Remove comment

--- ricty.rb
+++ ricty.rb
@@ -35,11 +35,6 @@ class Ricty < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test ricty_generator`.
     system "false"
   end
 end
