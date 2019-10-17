86120124a4e8bfd283d30876c6f5d33e81868761
equal-l2 <eng.equall2@gmail.com>
2019-10-17 22:19:32 +0900

    Update ricty_generator

--- ricty.rb
+++ ricty.rb
@@ -11,29 +11,16 @@ end
 
 class Ricty < Formula
   desc "Font for programming"
-  homepage "http://www.rs.tus.ac.jp/yyusa/ricty.html"
-  url "http://www.rs.tus.ac.jp/yyusa/ricty/ricty_generator-4.1.0.sh"
-  sha256 "6e2b656814ffdad5430f9c52bff89609b1350de1127f61966cdf441710ec60b3"
-  revision 2
+  homepage "http://yusa.lab.uec.ac.jp/~yusa/ricty.html"
+  url "http://yusa.lab.uec.ac.jp/~yusa/ricty/ricty_generator-4.1.1.sh"
+  sha256 "86bf0fed84ef806690b213798419405d7ca2a1a4bed4f6a28b87c2e2d07ad60d"
 
   option "with-powerline", "Patch for Powerline"
   option "without-fullwidth", "Disable fullwidth ambiguous characters"
   option "without-visible-space", "Disable visible zenkaku space"
   option "with-patch-in-place", "Patch Powerline glyphs directly into Ricty fonts without creating new 'for Powerline' fonts"
-  option "with-oblique", "make oblique fonts"
-
-  deprecated_option "powerline" => "with-powerline"
-  deprecated_option "disable-fullwidth" => "without-fullwidth"
-  deprecated_option "disable-visible-space" => "without-visible-space"
-  deprecated_option "patch-in-place" => "with-patch-in-place"
-  deprecated_option "oblique" => "with-oblique"
 
   depends_on "fontforge" => :build
-
-  resource "oblique_converter" do
-    url "http://www.rs.tus.ac.jp/yyusa/ricty/regular2oblique_converter.pe"
-    sha256 "365c7973a02abf3970f09a557f8f93065341885f9e13570fd2e901e530c4864d"
-  end
 
   resource "inconsolataregular" do
     url "https://github.com/google/fonts/raw/f0e90b27b6e567af9378952a37bc8cf29e2d88e9/ofl/inconsolata/Inconsolata-Regular.ttf"
@@ -52,20 +39,10 @@ class Ricty < Formula
     sha256 "d4c38664dd57bc5927abe8f4fbea8f06a8ece3fea49ea02354d4e03ac6d15006"
   end
 
-  patch do
-    # workaround for #43 and #46
-    url "https://raw.githubusercontent.com/sanemat/homebrew-font/cb5d2304f62226e3aa821a2563d3f7278342e2fe/ricty_generator.patch"
-    sha256 "c27000c9f76d07781254e9a9122b018ad74e7bb5e7df0c0961251b43f00c9b26"
-  end
-
   def install
     resource("migu1mfonts").stage { buildpath.install Dir["*"] }
     resource("inconsolataregular").stage { buildpath.install Dir["*"] }
     resource("inconsolatabold").stage { buildpath.install Dir["*"] }
-
-    if build.with? "oblique"
-      resource("oblique_converter").stage { buildpath.install Dir["*"] }
-    end
 
     if build.with? "powerline"
       powerline = Powerline.new
@@ -80,12 +57,6 @@ class Ricty < Formula
     ricty_args.unshift("-a") if build.without? "fullwidth"
 
     system "sh", "./ricty_generator-#{version}.sh", *ricty_args
-
-    if build.with? "oblique"
-      Dir["Ricty*.ttf"].each do |ttf|
-        system "fontforge", "-script", buildpath/"regular2oblique_converter.pe", ttf
-      end
-    end
 
     if build.with? "powerline"
       powerline_args = []
